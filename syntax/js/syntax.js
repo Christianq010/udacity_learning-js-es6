70b75cd1ec76c4b2a03ffd783347839346a77770
Christiaan Quyn <christianq010@gmail.com>
2017-10-31 22:30:27 +0530

    Feat: Add string template literate quiz solution

--- syntax/js/syntax.js
+++ syntax/js/syntax.js
@@ -17,3 +17,39 @@ function displayPosts() {
 }
 
 displayPosts();
+
+/*
+ * Programming Quiz: Build an HTML Fragment (1-2)
+ */
+
+const cheetah = {
+    name: 'Cheetah',
+    scientificName: 'Acinonyx jubatus',
+    lifespan: '10-12 years',
+    speed: '68-75 mph',
+    diet: 'carnivore',
+    summary: 'Fastest mammal on land, the cheetah can reach speeds of 60 or perhaps even 70 miles (97 or 113 kilometers) an hour over short distances. It usually chases its prey at only about half that speed, however. After a chase, a cheetah needs half an hour to catch its breath before it can eat.',
+    fact: 'Cheetahs have “tear marks” that run from the inside corners of their eyes down to the outside edges of their mouth.'
+};
+
+// creates an animal trading card
+function createAnimalTradingCardHTML(animal) {
+    const cardHTML = `<div class="card">
+        <h3 class="name"> ${cheetah.name} </h3>
+        <img src="${cheetah.name}.jpg" alt="${cheetah.name}" class="picture">
+        <div class="description">
+            <p class="fact"> ${cheetah.fact} </p>
+            <ul class="details">
+                <li><span class="bold">Scientific Name</span>: ${cheetah.scientificName}</li> +
+                <li><span class="bold">Average Lifespan</span>: ${cheetah.lifespan}</li>
+                <li><span class="bold">Average Speed</span>: ${cheetah.speed}</li>
+                <li><span class="bold">Diet</span>: ${cheetah.diet}</li>
+            </ul>
+            <p class="brief"> ${cheetah.summary} </p>
+        </div>
+    </div>`;
+
+    return cardHTML;
+}
+
+console.log(createAnimalTradingCardHTML(cheetah));
