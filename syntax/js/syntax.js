b915f1a2f0b8ecd873d9c5627fb510317600767a
Christiaan Quyn <christianq010@gmail.com>
2017-12-12 16:48:15 +0530

    Add rest parameter quiz

--- syntax/js/syntax.js
+++ syntax/js/syntax.js
@@ -141,3 +141,29 @@ console.log(produce);
 const order = [20.17, 18.67, 1.50, "cheese", "eggs", "milk", "bread"];
 const [total, subtotal, tax, ...items] = order;
 console.log(total, subtotal, tax, items);
+
+/*
+ * Programming Quiz: Using the Rest Parameter (1-5)
+ */
+
+// your code goes here
+
+function average(...nums) {
+
+    let total = 0;
+
+    for (const num of nums) {
+        total += num;
+    }
+
+    if (nums.length === 0) {
+        nums.length = 1;
+    }
+
+    return total / nums.length;
+}
+
+console.log(average(2, 6));
+console.log(average(2, 3, 3, 5, 7, 10));
+console.log(average(7, 1432, 12, 13, 100));
+console.log(average());
