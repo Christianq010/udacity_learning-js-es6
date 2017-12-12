be52cd7306a368854161d4a58e7c89195f5e6fda
Christiaan Quyn <christianq010@gmail.com>
2017-12-12 16:45:59 +0530

    feat: Add rest parameter quiz

--- syntax/js/syntax.js
+++ syntax/js/syntax.js
@@ -136,3 +136,8 @@ const vegetables = ["corn", "potatoes", "carrots"];
 const produce = [...fruits, ...vegetables];
 
 console.log(produce);
+
+// Rest parameter
+const order = [20.17, 18.67, 1.50, "cheese", "eggs", "milk", "bread"];
+const [total, subtotal, tax, ...items] = order;
+console.log(total, subtotal, tax, items);
