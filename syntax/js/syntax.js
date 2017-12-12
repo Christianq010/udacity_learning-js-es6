32c65cb91aa050cfa7ed0f19f08c96e3721ec761
Christiaan Quyn <christianq010@gmail.com>
2017-12-12 16:40:08 +0530

    Feat: Add the for of loop quiz with answer

--- syntax/js/syntax.js
+++ syntax/js/syntax.js
@@ -101,3 +101,27 @@ const gemstone = {
         // will calculate worth of gemstone based on type, color, and carat
     }
 };
+
+/*
+ *
+ * ----------- For of Loop -------------- | 
+ *
+ */
+
+const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (const digit of digits) {
+    console.log(digit);
+}
+
+/*
+ * Programming Quiz: Writing a For...of Loop (1-4)
+ */
+
+const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
+// your code goes here
+
+for (const day of days) {
+    console.log(day.charAt(0).toUpperCase() + day.slice(1));
+}
